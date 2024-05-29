40488832a9e6e5d95edf9a170eaa5b83491102ba
0Edu0 <edulink.1610@gmail.com>
2024-05-28 21:46:14 -0600

    Clase Cliente creado y Menu Principal creado

---
+++ Proyecto_Final_Progra/Cliente.cs
@@ -0,0 +1,69 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Proyecto_Final_Progra
+{
+    public class Cliente
+    {
+        public string Nombre;
+        public int DPI;
+        public int Celular;
+        public string Correo;
+        public int Tarjeta;
+        public string Ingreso;
+        public string Salida;
+        public int Acompañantes;
+        private string connectionString = "server=localhost;user=root;database=proyectofinal;port=3306;password=kevin";
+
+        public void agregar()
+        {
+
+            try
+            {
+                using (MySqlConnection conn = new MySqlConnection(connectionString))
+                {
+                    conn.Open();
+                    Console.WriteLine("Nota: Ingresa la fecha de esta manera (1 de enero de 2024)");
+                    Console.WriteLine("Ingresa tu numero de DPI.");
+                    int DPICliente = Convert.ToInt32(Console.ReadLine());
+                    Console.WriteLine("Ingresa tu nombre completo.");
+                    string NombreCliente = Console.ReadLine();
+                    Console.WriteLine("Ingresa tu numero de celular.");
+                    int CelularCliente = Convert.ToInt32(Console.ReadLine());
+                    Console.WriteLine("Ingresa tu correo electronico.");
+                    string CorreoCliente = Console.ReadLine();
+                    Console.WriteLine("Ingresa los digitos de tu tarjeta de debito o credito.");
+                    int TarjetaCliente = Convert.ToInt32(Console.ReadLine());
+                    Console.WriteLine("Fecha de tu ingreso al hotel.");
+                    string IngresoCliente = Console.ReadLine();
+                    Console.WriteLine("Fecha de salida.");
+                    string SalidaCliente = Console.ReadLine();
+                    Console.WriteLine("Ingresa el numero de acompañantes.");
+                    int AcompañantesCliente = Convert.ToInt32(Console.ReadLine());
+                    Console.WriteLine("Datos ingresados exitosamente.");
+
+                    string query = "INSERT INTO cliente (DPI,Nombre,Celular,CorreoElectronico,Tarjeta,FechaIngreso,FechaSalida,Acompañantes) VALUES (´" + DPICliente + "´,´" + NombreCliente + "´,´" + CelularCliente + "´,´" + CorreoCliente + "´,´" + TarjetaCliente + "´,´" + IngresoCliente + "´,´" + SalidaCliente + "´,´" + AcompañantesCliente + "´);";
+                    MySqlCommand cmd = new MySqlCommand(query, conn);
+
+                    MySqlDataReader reader = cmd.ExecuteReader();
+                    reader.Close();
+                    Console.WriteLine("Registro guardado");
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("ERROR: " + ex);
+
+            }
+        }
+
+        public void Consultar()
+        {
+            Console.WriteLine("Ingresa el codigo de reservacion:");
+        }
+    }
+}
--- Proyecto_Final_Progra/Program.cs
+++ Proyecto_Final_Progra/Program.cs
@@ -3,13 +3,61 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+using MySqlX.XDevAPI;
 
 namespace Proyecto_Final_Progra
 {
-    internal class Program
+    class Program
     {
         static void Main(string[] args)
         {
+            int opcion;
+            bool continuar = true;
+            Cliente cliente = new Cliente();
+            while (continuar)
+            {
+                Console.WriteLine("Menu de reservaciones");
+                Console.WriteLine("1. Crear reservacion");
+                Console.WriteLine("2. Consultar reservaciones");
+                Console.WriteLine("3. Actualizar reservacion");
+                Console.WriteLine("4. Eliminar reservacion");
+                Console.WriteLine("5. Salir del menu");
+                Console.WriteLine("Ingresar una opcion:");
+
+                if (!int.TryParse(Console.ReadLine(), out opcion))
+                {
+                    Console.WriteLine("Opción no válida. Por favor ingrese un número válido.");
+                    continue;
+                }
+                else
+                {
+
+                    switch (opcion)
+                    {
+                        case 1:
+                            Console.WriteLine("Ingresa tus datos");
+                            cliente.agregar();
+                            break;
+                        case 2:
+                            Console.WriteLine("Consultar reservaciones...");
+                            break;
+                        case 3:
+                            Console.WriteLine("Actualizar reservación...");
+                            break;
+                        case 4:
+                            Console.WriteLine("Eliminar reservación...");
+                            break;
+                        case 5:
+                            continuar = false;
+                            break;
+                        default:
+                            Console.WriteLine("Opción no válida. Por favor seleccione una opción del menú.");
+                            break;
+                    }
+                }
+            }
+            Console.WriteLine("Saliendo del programa...");
         }
     }
 }
